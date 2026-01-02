0e0dabfdbcb325d2de9b8c987f7a1b137ba090d9
lammai442 <lam.mai442@gmail.com>
2026-01-02 14:29:07 +0100

    Succesfull bcrypt

--- backend/utils/bcrypt.py
+++ backend/utils/bcrypt.py
@@ -1,7 +1,16 @@
-import bcrypt
+from passlib.hash import bcrypt
 
 
+# Hasha lösenord
 def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed_psw = bcrypt.hashpw(password, salt)
-    return hashed_psw
+    return bcrypt.hash(password)
+
+
+# Verifiera lösenord
+def verify_password(password: str, hashed: str) -> bool:
+    return bcrypt.verify(password, hashed)
+
+
+# Verifiera lösenord
+def verify_password(password: str, hashed: str) -> bool:
+    return bcrypt.verify(password, hashed)
