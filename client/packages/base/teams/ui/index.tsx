bce490e848645ed39960f70768bd140f6117c57b
lammai442 <lam.mai442@gmail.com>
2026-03-12 16:28:23 +0100

    Add join team message

--- client/packages/base/teams/ui/index.tsx
+++ client/packages/base/teams/ui/index.tsx
@@ -1,16 +1,31 @@
 import type { Team } from '@fishScore/eventsdata';
-import { Flex, Stack, Text, Title } from '@mantine/core';
-
+import { Box, Button, Flex, Stack, Text, Title } from '@mantine/core';
+import { IconTrophy } from '@tabler/icons-react';
 type Props = {
 	team: Team;
 	userId: string | undefined;
 	rankNr: number;
+	userExistInTeam: boolean | undefined;
 };
 
-export const Teams = ({ team, userId, rankNr }: Props) => {
-	const userExistInTeam = team.members.some(
-		(member) => member.userId === userId,
-	);
+export const Teams = ({ team, userId, rankNr, userExistInTeam }: Props) => {
+	// const userExistInTeam = team.members.some(
+	// 	(member) => member.userId === userId,
+	// );
+
+	let trophyColor = '';
+
+	switch (rankNr) {
+		case 1:
+			trophyColor = 'var(--bg-gold-color)';
+			break;
+		case 2:
+			trophyColor = 'var(--bg-silver-color)';
+			break;
+		case 3:
+			trophyColor = 'var(--bg-bronze-color)';
+			break;
+	}
 
 	return (
 		<>
@@ -18,49 +33,59 @@ export const Teams = ({ team, userId, rankNr }: Props) => {
 				key={team.createdAt}
 				bdrs={'15px'}
 				bd={
-					userExistInTeam
-						? '1px solid var(--br-blue)'
+					rankNr <= 3
+						? `2px solid ${trophyColor}`
 						: '1px solid var(--br-grey)'
 				}
-				bg={
-					userExistInTeam
-						? 'var(--bg-blue-color)'
-						: 'var(--bg-white-color)'
-				}
+				style={{
+					borderLeft:
+						rankNr <= 3
+							? `6px solid ${trophyColor}`
+							: '1px solid var(--br-grey)',
+				}}
 				p={'20px'}>
-				<Flex gap={'0.5rem'}>
-					<Text
-						span
-						fw={700}
-						bg='var(--bg-medium-light-grey-color)'
-						w={30}
-						h={30}
-						style={{
-							display: 'inline-flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							borderRadius: '50%',
-						}}>
-						{rankNr}
-					</Text>
+				<Flex justify={'space-between'} align={'center'}>
 					<Flex gap={'0.5rem'}>
-						<Title order={3}>{team.teamName}</Title>
-						{userExistInTeam && (
-							<Text
-								bg={'var(--bg-medium-light-grey-color)'}
-								p={'0.4rem'}
-								fz={'sm'}
-								bdrs={'0.5rem'}>
-								Your team
-							</Text>
-						)}
+						<Text
+							span
+							fw={700}
+							bg='var(--bg-medium-light-grey-color)'
+							w={30}
+							h={30}
+							style={{
+								display: 'inline-flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+								borderRadius: '50%',
+							}}>
+							{rankNr}
+						</Text>
+						<Flex gap={'0.5rem'}>
+							<Title order={3}>{team.teamName}</Title>
+							{userExistInTeam && (
+								<Text
+									bg={'var(--bg-medium-light-grey-color)'}
+									p={'0.4rem'}
+									fz={'sm'}
+									bdrs={'0.5rem'}>
+									Your team
+								</Text>
+							)}
+						</Flex>
 					</Flex>
+					{rankNr <= 3 && (
+						<IconTrophy color={trophyColor}></IconTrophy>
+					)}
 				</Flex>
 				<Text>
 					Medlemmar:{' '}
 					{team.members.map((member) => member.name).join(', ')}
 				</Text>
-				<Text fz={'h2'}>{team.totalCatchWeight} kg</Text>
+				<Text fz={'xl'}>Totalt: {team.totalCatchWeight} kg</Text>
+
+				{!userExistInTeam && (
+					<Button bg={'var(--bg-black-color)'}>Join team</Button>
+				)}
 			</Stack>
 		</>
 	);
