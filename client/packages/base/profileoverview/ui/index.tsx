494968c465d3bac141d1b7716505f903d713dc54
lammai442 <lam.mai442@gmail.com>
2026-03-19 08:14:40 +0100

    Added states for profileoverview

--- client/packages/base/profileoverview/ui/index.tsx
+++ client/packages/base/profileoverview/ui/index.tsx
@@ -3,11 +3,34 @@ import { Button } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { showNotification } from '@mantine/notifications';
 import { IconX } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { fetchUserProfile } from '@fishScore/apiuser';
+import { Loading } from '@fishScore/loading';
+import { User } from '@fishScore/usersdata/data';
 type Props = {};
 
 export const ProfileOverview = ({}: Props) => {
+	const [loading, setLoading] = useState<boolean>(false);
+	const [user, setUser] = useState<User | null>(null);
+	const [userStats, setUserStats] = useState<null>(null);
+	const [userCatches, setUserCatches] = useState<null>(null);
 	const navigate = useNavigate();
 
+	useEffect(() => {
+		const getUserProfile = async () => {
+			setLoading(true);
+			const response = await fetchUserProfile();
+			setLoading(false);
+			if (response.success) {
+				setUser(response.data.userProfile.user);
+				setUserStats(response.data.userProfile.stats);
+				setUserCatches(response.data.userProfile.catches);
+			}
+		};
+
+		getUserProfile();
+	}, []);
+	console.log(userCatches);
 	const handleLogout = async () => {
 		const response = await fetchLogout();
 		if (response.success) {
@@ -24,7 +47,8 @@ export const ProfileOverview = ({}: Props) => {
 	};
 	return (
 		<>
-			<div>profileOverview</div>
+			<Loading visible={loading} text='Getting profile'></Loading>
+			<div>{user?.firstName}</div>
 			<Button onClick={() => handleLogout()}>Log out</Button>
 		</>
 	);
--- client/packages/pages/profilepage/ui/index.tsx
+++ client/packages/pages/profilepage/ui/index.tsx
@@ -1,27 +1,25 @@
-import { fetchLogout } from '@fishScore/apiauth';
-import { Button, Stack } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
+import { Stack } from '@mantine/core';
 import { PageHeader } from '@fishScore/pageheader';
 import { useEffect, useState } from 'react';
-import { fetchUserMe, fetchUserStats } from '@fishScore/apiuser';
+import { fetchUserProfile } from '@fishScore/apiuser';
 import { Loading } from '@fishScore/loading';
 import { ProfileOverview } from '../../../base/profileoverview/ui';
 
 export const ProfilePage = () => {
 	const [loading, setLoading] = useState(false);
-	const [userStat, setUserStat] = useState(null);
-	useEffect(() => {
-		const getUser = async () => {
-			setLoading(true);
-			const response = await fetchUserStats();
-			setLoading(false);
-			if (response.success) {
-				setUserStat(response.data.userStats);
-			}
-		};
+	// const [userProfile, setUserProfile] = useState(null);
+	// useEffect(() => {
+	// 	const getUser = async () => {
+	// 		setLoading(true);
+	// 		const response = await fetchUserProfile();
+	// 		setLoading(false);
+	// 		if (response.success) {
+	// 			setUserProfile(response.data.userProfile);
+	// 		}
+	// 	};
 
-		getUser();
-	}, []);
+	// 	getUser();
+	// }, []);
 
 	return (
 		<Stack>
