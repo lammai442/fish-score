c3deda05d47d0fd655bb58284abe4010752f1d49
lammai442 <lam.mai442@gmail.com>
2026-03-20 08:55:55 +0100

    Reversed profilepage

--- client/packages/pages/profilepage/ui/index.tsx
+++ client/packages/pages/profilepage/ui/index.tsx
@@ -1,29 +1,10 @@
 import { Stack } from '@mantine/core';
 import { PageHeader } from '@fishScore/pageheader';
-import { useEffect, useState } from 'react';
-import { fetchUserProfile } from '@fishScore/apiuser';
-import { Loading } from '@fishScore/loading';
 import { ProfileOverview } from '../../../base/profileoverview/ui';
 
 export const ProfilePage = () => {
-	const [loading, setLoading] = useState(false);
-	// const [userProfile, setUserProfile] = useState(null);
-	// useEffect(() => {
-	// 	const getUser = async () => {
-	// 		setLoading(true);
-	// 		const response = await fetchUserProfile();
-	// 		setLoading(false);
-	// 		if (response.success) {
-	// 			setUserProfile(response.data.userProfile);
-	// 		}
-	// 	};
-
-	// 	getUser();
-	// }, []);
-
 	return (
 		<Stack>
-			<Loading visible={loading} text='Getting profile'></Loading>
 			<PageHeader title='Profile' />
 			<ProfileOverview></ProfileOverview>
 		</Stack>
