bb90d3354658dc5f3f948730fc711db0781d1990
lammai442 <lam.mai442@gmail.com>
2026-03-26 09:14:01 +0100

    Refactor EventTabs

--- client/packages/pages/eventpage/ui/index.tsx
+++ client/packages/pages/eventpage/ui/index.tsx
@@ -30,7 +30,8 @@ import { FishCatchCard } from '@fishScore/fishcatchcard';
 import { AddCatch } from '@fishScore/addcatch';
 import { showNotification } from '@mantine/notifications';
 import { EventsMessage } from '@fishScore/eventsmessage';
-import { fetchAddMessage } from '../../../core/api/apimessages/data';
+import { EventTabs } from '../../../base/eventtabs/ui';
+// import { fetchAddMessage } from '../../../core/api/apimessages/data';
 
 export const EventPage = () => {
 	const { eventId } = useParams();
@@ -42,7 +43,7 @@ export const EventPage = () => {
 	const [leaderboard, setLeaderboard] = useState<Team[]>([]);
 	const [activity, setActivity] = useState<FishCatch[]>([]);
 	const [loading, setLoading] = useState<boolean>(false);
-	const [message, setMessage] = useState<string>('');
+	// const [message, setMessage] = useState<string>('');
 	const { user } = useUserStore();
 
 	// Kontroll om user finns i team
@@ -129,28 +130,28 @@ export const EventPage = () => {
 		}
 	};
 
-	const handleSendMsg = async () => {
-		try {
-			setLoading(true);
+	// const handleSendMsg = async () => {
+	// 	try {
+	// 		setLoading(true);
 
-			const response = await fetchAddMessage(
-				currentEvent?.eventId,
-				message,
-			);
-			if (response.success) {
-				setMessage('');
-				showNotification({
-					title: 'Message',
-					message: 'Your new message has been published!',
-					color: 'var(--bg-primary)',
-					icon: <IconCheck />,
-					position: 'top-center',
-				});
-			}
-		} finally {
-			setLoading(false);
-		}
-	};
+	// 		const response = await fetchAddMessage(
+	// 			currentEvent?.eventId,
+	// 			message,
+	// 		);
+	// 		if (response.success) {
+	// 			setMessage('');
+	// 			showNotification({
+	// 				title: 'Message',
+	// 				message: 'Your new message has been published!',
+	// 				color: 'var(--bg-primary)',
+	// 				icon: <IconCheck />,
+	// 				position: 'top-center',
+	// 			});
+	// 		}
+	// 	} finally {
+	// 		setLoading(false);
+	// 	}
+	// };
 
 	return (
 		<>
@@ -276,158 +277,167 @@ export const EventPage = () => {
 
 			{/* Leaderboard/Activity tab  */}
 			{!loading && currentEvent && (
-				<Tabs
-					variant='pills'
-					value={mode}
-					onChange={setMode}
-					classNames={{
-						tab: 'scoreboard__tab',
-						list: 'scoreboard__list',
-					}}>
-					<Tabs.List
-						grow
-						justify='center'
-						p={'0.2rem'}
-						bg={'var(--bg-muted)'}
-						bdrs={15}>
-						<Tabs.Tab value='leaderboard'>Leaderboard</Tabs.Tab>
-						<Tabs.Tab value='activity'>Activity</Tabs.Tab>
-						<Tabs.Tab value='messages'>Messages</Tabs.Tab>
-					</Tabs.List>
-					{/* Leaderboard tab */}
-					<Tabs.Panel value='leaderboard' pt='md'>
-						<Stack
-							style={{
-								maxHeight: '550px',
-								overflow: 'auto',
-								scrollbarWidth: 'thin',
-							}}>
-							{currentEvent && leaderboard.length > 0 ? (
-								leaderboard.map((team: Team, index) => {
-									return (
-										<Teams
-											key={team.teamId}
-											team={team}
-											userId={user?.userId}
-											rankNr={index + 1}
-											userIsInAnyTeam={!!usersTeam}
-											eventId={eventId}
-											createdBy={
-												currentEvent.createdBy
-											}></Teams>
-									);
-								})
-							) : (
-								<Text ta={'center'}>
-									No teams has been created
-								</Text>
-							)}
-						</Stack>
-					</Tabs.Panel>
-					{/* Activity tab */}
-					<Tabs.Panel value='activity' pt='md'>
-						<Stack
-							style={{
-								maxHeight: '550px',
-								overflow: 'auto',
-								scrollbarWidth: 'thin',
-							}}>
-							{activity.length > 0 ? (
-								activity.map((fishCatch) => {
-									return (
-										<FishCatchCard
-											key={fishCatch.catchId}
-											fishCatch={fishCatch}
-											eventStatus={currentEvent.status}
-											userId={user?.userId}
-											variant='activityCatch'></FishCatchCard>
-									);
-								})
-							) : (
-								<Text ta={'center'}>
-									No fish has been caught! Who will be the
-									first one?
-								</Text>
-							)}
-						</Stack>
-					</Tabs.Panel>
-					{/* Messages tab */}
-					<Tabs.Panel value='messages' pt='md'>
-						<Stack>
-							<Flex gap={'sm'}>
-								<Textarea
-									placeholder='New message'
-									autosize
-									maxRows={4}
-									flex={9}
-									value={message}
-									onChange={(event) =>
-										setMessage(event.currentTarget.value)
-									}
-									styles={{
-										input: {
-											'--input-bd-focus':
-												'var(--color-black)',
-										},
-									}}></Textarea>
-								<Button
-									flex={1}
-									bg={
-										!message.trim()
-											? 'var(--color-grey)'
-											: 'var(--color-black)'
-									}
-									fz={'0.68rem'}
-									disabled={!message.trim()}
-									onClick={handleSendMsg}>
-									Send
-								</Button>
-							</Flex>
+				<EventTabs
+					mode={mode}
+					setMode={setMode}
+					currentEvent={currentEvent}
+					leaderboard={leaderboard}
+					user={user}
+					usersTeam={usersTeam}
+					activity={activity}
+					setLoading={setLoading}></EventTabs>
+				// <Tabs
+				// 	variant='pills'
+				// 	value={mode}
+				// 	onChange={setMode}
+				// 	classNames={{
+				// 		tab: 'scoreboard__tab',
+				// 		list: 'scoreboard__list',
+				// 	}}>
+				// 	<Tabs.List
+				// 		grow
+				// 		justify='center'
+				// 		p={'0.2rem'}
+				// 		bg={'var(--bg-muted)'}
+				// 		bdrs={15}>
+				// 		<Tabs.Tab value='leaderboard'>Leaderboard</Tabs.Tab>
+				// 		<Tabs.Tab value='activity'>Activity</Tabs.Tab>
+				// 		<Tabs.Tab value='messages'>Messages</Tabs.Tab>
+				// 	</Tabs.List>
+				// 	{/* Leaderboard tab */}
+				// 	<Tabs.Panel value='leaderboard' pt='md'>
+				// 		<Stack
+				// 			style={{
+				// 				maxHeight: '550px',
+				// 				overflow: 'auto',
+				// 				scrollbarWidth: 'thin',
+				// 			}}>
+				// 			{currentEvent && leaderboard.length > 0 ? (
+				// 				leaderboard.map((team: Team, index) => {
+				// 					return (
+				// 						<Teams
+				// 							key={team.teamId}
+				// 							team={team}
+				// 							userId={user?.userId}
+				// 							rankNr={index + 1}
+				// 							userIsInAnyTeam={!!usersTeam}
+				// 							eventId={eventId}
+				// 							createdBy={
+				// 								currentEvent.createdBy
+				// 							}></Teams>
+				// 					);
+				// 				})
+				// 			) : (
+				// 				<Text ta={'center'}>
+				// 					No teams has been created
+				// 				</Text>
+				// 			)}
+				// 		</Stack>
+				// 	</Tabs.Panel>
+				// 	{/* Activity tab */}
+				// 	<Tabs.Panel value='activity' pt='md'>
+				// 		<Stack
+				// 			style={{
+				// 				maxHeight: '550px',
+				// 				overflow: 'auto',
+				// 				scrollbarWidth: 'thin',
+				// 			}}>
+				// 			{activity.length > 0 ? (
+				// 				activity.map((fishCatch) => {
+				// 					return (
+				// 						<FishCatchCard
+				// 							key={fishCatch.catchId}
+				// 							fishCatch={fishCatch}
+				// 							eventStatus={currentEvent.status}
+				// 							userId={user?.userId}
+				// 							variant='activityCatch'></FishCatchCard>
+				// 					);
+				// 				})
+				// 			) : (
+				// 				<Text ta={'center'}>
+				// 					No fish has been caught! Who will be the
+				// 					first one?
+				// 				</Text>
+				// 			)}
+				// 		</Stack>
+				// 	</Tabs.Panel>
+				// 	{/* Messages tab */}
+				// 	<Tabs.Panel value='messages' pt='md'>
+				// 		<Stack>
+				// 			<Flex gap={'sm'}>
+				// 				<Textarea
+				// 					placeholder='New message'
+				// 					autosize
+				// 					maxRows={4}
+				// 					flex={9}
+				// 					value={message}
+				// 					onChange={(event) =>
+				// 						setMessage(event.currentTarget.value)
+				// 					}
+				// 					styles={{
+				// 						input: {
+				// 							'--input-bd-focus':
+				// 								'var(--color-black)',
+				// 						},
+				// 					}}></Textarea>
+				// 				<Button
+				// 					flex={1}
+				// 					bg={
+				// 						!message.trim()
+				// 							? 'var(--color-grey)'
+				// 							: 'var(--color-black)'
+				// 					}
+				// 					fz={'0.68rem'}
+				// 					disabled={!message.trim()}
+				// 					onClick={handleSendMsg}>
+				// 					Send
+				// 				</Button>
+				// 			</Flex>
 
-							{currentEvent.messages.length > 0 && (
-								<Stack
-									gap={'sm'}
-									bd={'1px solid var(--color-grey-dark)'}
-									p={'sm'}
-									bdrs={'lg'}
-									style={{
-										maxHeight: '550px',
-										overflow: 'auto',
-										scrollbarWidth: 'thin',
-									}}>
-									{currentEvent.messages.map(
-										(message, index) => {
-											const isLast =
-												index ===
-												currentEvent.messages.length -
-													1;
-											const hasMultiple =
-												currentEvent.messages.length >
-												1;
+				// 			{currentEvent.messages.length > 0 && (
+				// 				<Stack
+				// 					gap={'sm'}
+				// 					bd={'1px solid var(--color-grey-dark)'}
+				// 					p={'sm'}
+				// 					bdrs={'lg'}
+				// 					style={{
+				// 						maxHeight: '550px',
+				// 						overflow: 'auto',
+				// 						scrollbarWidth: 'thin',
+				// 					}}>
+				// 					{currentEvent.messages.map(
+				// 						(message, index) => {
+				// 							const isLast =
+				// 								index ===
+				// 								currentEvent.messages.length -
+				// 									1;
+				// 							const hasMultiple =
+				// 								currentEvent.messages.length >
+				// 								1;
 
-											return (
-												<Stack
-													gap={'sm'}
-													key={message.messageId}>
-													<EventsMessage
-														eventMessage={
-															message
-														}></EventsMessage>
-													{hasMultiple && !isLast && (
-														<Divider
-															key={index}
-															size={'sm'}
-														/>
-													)}
-												</Stack>
-											);
-										},
-									)}
-								</Stack>
-							)}
-						</Stack>
-					</Tabs.Panel>
-				</Tabs>
+				// 							return (
+				// 								<Stack
+				// 									gap={'sm'}
+				// 									key={message.messageId}>
+				// 									<EventsMessage
+				// 										eventMessage={
+				// 											message
+				// 										}></EventsMessage>
+				// 									{hasMultiple && !isLast && (
+				// 										<Divider
+				// 											key={index}
+				// 											size={'sm'}
+				// 										/>
+				// 									)}
+				// 								</Stack>
+				// 							);
+				// 						},
+				// 					)}
+				// 				</Stack>
+				// 			)}
+				// 		</Stack>
+				// 	</Tabs.Panel>
+				// </Tabs>
 			)}
 		</>
 	);
