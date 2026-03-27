2b42519d06cb0416615797aed6773e525aa8bd3c
lammai442 <lam.mai442@gmail.com>
2026-03-27 14:54:19 +0100

    Chanage to loading

--- client/packages/base/authform/ui/index.tsx
+++ client/packages/base/authform/ui/index.tsx
@@ -70,108 +70,109 @@ export const AuthForm = () => {
 	};
 
 	return (
-		<Box
-			maw={400}
-			mx='auto'
-			mt={50}
-			bg='white'
-			p={16}
-			style={{
-				borderRadius: '10px',
-				boxShadow:
-					'0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
-				position: 'relative',
-			}}>
+		<>
 			<Loading
 				visible={loading}
 				text={
 					mode === 'login' ? 'Signing in...' : 'Register new user...'
 				}
 			/>
-
-			<Center mb='md'>
-				<Image src='/transparent-logo.png' w={80} h={80} />
-			</Center>
-			<Title order={2} ta='center' mb='lg'>
-				FishScore
-			</Title>
-			<Text>Track your fishing competition with ease</Text>
-			<SegmentedControl
-				value={mode}
-				onChange={(value) => setMode(value as 'login' | 'register')}
-				data={[
-					{ label: 'Logga in', value: 'login' },
-					{ label: 'Registera', value: 'register' },
-				]}
-				fullWidth
-				styles={{
-					label: {
-						fontWeight: '700',
-					},
-					root: {
-						padding: '0.4rem',
-						borderRadius: '1rem',
-					},
-					control: {
-						borderRadius: '1rem',
-						overflow: 'hidden',
-					},
-					indicator: {
-						borderRadius: '1rem',
-					},
-				}}
-			/>
-			<form
-				onSubmit={form.onSubmit((values) => {
-					if (mode === 'register') {
-						handleRegister(values as RegisterData);
-					} else {
-						const loginData: LoginData = {
-							email: values.email,
-							password: values.password,
-						};
-						handleLogin(loginData);
-					}
-				})}>
-				<TextInput
-					label='Email'
-					placeholder='din@email.se'
-					{...form.getInputProps('email')}
+			<Box
+				maw={400}
+				mx='auto'
+				mt={50}
+				bg='white'
+				p={16}
+				style={{
+					borderRadius: '10px',
+					boxShadow:
+						'0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+					position: 'relative',
+				}}>
+				<Center mb='md'>
+					<Image src='/transparent-logo.png' w={80} h={80} />
+				</Center>
+				<Title order={2} ta='center' mb='lg'>
+					FishScore
+				</Title>
+				<Text>Track your fishing competition with ease</Text>
+				<SegmentedControl
+					value={mode}
+					onChange={(value) => setMode(value as 'login' | 'register')}
+					data={[
+						{ label: 'Logga in', value: 'login' },
+						{ label: 'Registera', value: 'register' },
+					]}
+					fullWidth
 					styles={{
 						label: {
-							fontWeight: 700,
+							fontWeight: '700',
+						},
+						root: {
+							padding: '0.4rem',
+							borderRadius: '1rem',
+						},
+						control: {
+							borderRadius: '1rem',
+							overflow: 'hidden',
+						},
+						indicator: {
+							borderRadius: '1rem',
 						},
 					}}
 				/>
-				{mode === 'register' && (
-					<>
-						<TextInput
-							label='Förnamn'
-							mt='md'
-							{...form.getInputProps('firstName')}
-							styles={{
-								label: {
-									fontWeight: 700,
-								},
-							}}
-						/>
-						<TextInput
-							label='Efternamn'
-							mt='md'
-							{...form.getInputProps('lastName')}
-						/>
-					</>
-				)}
-				<PasswordInput
-					label='Lösenord'
-					placeholder='Ditt lösenord'
-					mt='md'
-					{...form.getInputProps('password')}
-				/>
-				<Button type='submit' fullWidth mt='xl'>
-					{mode === 'login' ? 'Logga in' : 'Registrera'}
-				</Button>
-			</form>
-		</Box>
+				<form
+					onSubmit={form.onSubmit((values) => {
+						if (mode === 'register') {
+							handleRegister(values as RegisterData);
+						} else {
+							const loginData: LoginData = {
+								email: values.email,
+								password: values.password,
+							};
+							handleLogin(loginData);
+						}
+					})}>
+					<TextInput
+						label='Email'
+						placeholder='din@email.se'
+						{...form.getInputProps('email')}
+						styles={{
+							label: {
+								fontWeight: 700,
+							},
+						}}
+					/>
+					{mode === 'register' && (
+						<>
+							<TextInput
+								label='Förnamn'
+								mt='md'
+								{...form.getInputProps('firstName')}
+								styles={{
+									label: {
+										fontWeight: 700,
+									},
+								}}
+							/>
+							<TextInput
+								label='Efternamn'
+								mt='md'
+								{...form.getInputProps('lastName')}
+							/>
+						</>
+					)}
+					<PasswordInput
+						label='Lösenord'
+						placeholder='Ditt lösenord'
+						mt='md'
+						{...form.getInputProps('password')}
+					/>
+					<Button type='submit' fullWidth mt='xl'>
+						{mode === 'login' ? 'Logga in' : 'Registrera'}
+					</Button>
+				</form>
+			</Box>
+		</>
 	);
 };
--- client/packages/base/loading/ui/index.tsx
+++ client/packages/base/loading/ui/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Loader, LoadingOverlay, Stack, Title } from '@mantine/core';
+import { Center, Image, Loader, Stack, Title } from '@mantine/core';
 
 type Props = {
 	visible: boolean;
@@ -6,23 +6,26 @@ type Props = {
 };
 
 export const Loading = ({ visible, text }: Props) => {
+	if (!visible) return null;
+
 	return (
-		<LoadingOverlay
-			visible={visible}
-			zIndex={1000}
-			loaderProps={{
-				children: (
-					<Stack align='center' gap={'sm'}>
-						<Image
-							w={'150px'}
-							fit='contain'
-							src={'/transparent-logo.png'}></Image>
-						<Loader color={'var(--color-primary)'} />
-						<Title order={4}>{text}</Title>
-					</Stack>
-				),
-			}}
-			overlayProps={{ radius: 'sm', blur: 2 }}
-		/>
+		<Center
+			style={{
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100vw',
+				height: '100vh',
+				zIndex: 1001,
+				backgroundColor: 'var(--color-white)',
+				padding: '1rem',
+				textAlign: 'center',
+			}}>
+			<Stack align='center' gap='sm'>
+				<Image w={150} fit='contain' src='/transparent-logo.png' />
+				<Loader color='var(--color-primary)' />
+				<Title order={4}>{text}</Title>
+			</Stack>
+		</Center>
 	);
 };
