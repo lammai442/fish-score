2b8e361c5885265840fc88f00d17c2f81ce5992c
lammai442 <lam.mai442@gmail.com>
2026-03-30 08:07:39 +0200

    Add content to error page

--- client/packages/pages/errorpage/ui/index.tsx
+++ client/packages/pages/errorpage/ui/index.tsx
@@ -1,3 +1,29 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { Button, Stack, Text, Title } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
 export const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	const navigate = useNavigate();
+
+	return (
+		<Stack align='center' ta={'center'}>
+			<DotLottieReact
+				src='https://lottie.host/c0a7b5a7-235f-43a3-9cc5-cdf84fb97c6f/VL3CVXiNfi.lottie'
+				loop
+				autoplay
+				speed={0.75}
+				style={{ width: 200, height: 200 }}
+			/>
+			<Title order={1}>404</Title>
+			<Text>
+				Oh no, it seems that the page you are looking for doesn't exist.
+			</Text>
+			<Button
+				bg={'var(--btn-primary-bg)'}
+				c={'var(--text-inverse'}
+				onClick={() => navigate('/', { replace: true })}>
+				To home
+			</Button>
+		</Stack>
+	);
 };
